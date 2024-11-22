87a4f351e29623e4b9f33ac00bf252459a7955f9
Saandy <38919673+KaelVNode@users.noreply.github.com>
2024-11-22 17:56:32 +0700

    Update Crot.sh

--- Crot.sh
+++ Crot.sh
@@ -2,28 +2,28 @@
 
 # Menampilkan ASCII art untuk "Saandy"
 echo ".d8888.  .d8b.   .d8b.  d8b   db d8888b. db    db "
-echo "88'  YP d8' `8b d8' `8b 888o  88 88  `8D `8b  d8' "
-echo "`8bo.   88ooo88 88ooo88 88V8o 88 88   88  `8bd8'  "
-echo "  `Y8b. 88~~~88 88~~~88 88 V8o88 88   88    88    "
+echo "88'  YP d8' \`8b d8' \`8b 888o  88 88  \`8D \`8b  d8' "
+echo "\`8bo.   88ooo88 88ooo88 88V8o 88 88   88  \`8bd8'  "
+echo "  \`Y8b. 88~~~88 88~~~88 88 V8o88 88   88    88    "
 echo "db   8D 88   88 88   88 88  V888 88  .8D    88    "
-echo "`8888Y' YP   YP YP   YP VP   V8P Y8888D'    YP    "
+echo "\`8888Y' YP   YP YP   YP VP   V8P Y8888D'    YP    "
 
 # Memeriksa apakah Docker sudah terinstal
 if ! command -v docker &> /dev/null; then
     echo "Docker tidak terinstal. Memulai proses instalasi..."
-    
+
     # Memperbarui paket dan menginstal dependensi
-    sudo apt-get update
+    sudo apt-get update -y
     sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
     # Menambahkan GPG key resmi Docker
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg || { echo "Gagal menambahkan GPG key"; exit 1; }
 
     # Menambahkan repository Docker
     echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     # Memperbarui paket dan menginstal Docker
-    sudo apt-get update
+    sudo apt-get update -y
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
     # Memeriksa apakah instalasi Docker berhasil
